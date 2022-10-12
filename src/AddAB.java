eaf75d0ba7eae2d63e78a6bc439692944592a299
sol1230 <solllll1230@gmail.com>
2022-10-12 13:19:52 +0900

    first

---
+++ src/AddAB.java
@@ -0,0 +1,18 @@
+//문제
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+// https://www.acmicpc.net/problem/1000
+import java.util.Scanner;
+public class AddAB{
+    public static void main(String[] args){
+//입력
+//첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+//출력
+//첫째 줄에 A+B를 출력한다.
+       System.out.println(A+B);
+        sc.close();
+    }
+}
---
+++ src/MinusAB.java
@@ -0,0 +1,22 @@
+
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+// https://www.acmicpc.net/problem/1001
+import java.util.Scanner;
+
+public class MinusAB {
+    public static void main(String[] args) {
+        
+// 입력
+// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+    Scanner sc = new Scanner(System.in);
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+
+// 출력
+// 첫째 줄에 A-B를 출력한다.
+    System.out.println(A-B);
+    sc.close();
+    }
+}
+
